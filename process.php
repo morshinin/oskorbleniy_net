7139d9d66bf461411f54931aaa39388d9b8a9a12
m.morshinin <m.morshinin@b2b-center.ru>
2021-01-30 21:13:33 +0300

    main add try catch

--- process.php
+++ process.php
@@ -3,8 +3,13 @@ include_once('db.php');
 
 $text = $_REQUEST['about'];
 
-$sql = "INSERT INTO posts (text) VALUES (':text')";
-$stmt = $pdo->prepare($sql);
-$stmt->execute(['text' => $text]);
+try {
+    $sql = "INSERT INTO posts (text) VALUES (':text')";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['text' => $text]);
+}
+catch (PDOException $e) {
+    echo 'Something went wrong: '. $e;
+}
 
 header('Location: /');
