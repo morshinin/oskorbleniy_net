07f0da9657507f0ee33039603b2a38077790ddf1
m.morshinin <m.morshinin@b2b-center.ru>
2021-01-31 22:01:08 +0300

    main update pagination

--- index.php
+++ index.php
@@ -38,20 +38,22 @@ $total_pages = ceil($total_rows / $no_of_records_per_page);
         <div>
             <p class="text-sm text-gray-700">
                 <span class="font-medium"><?= $no_of_records_per_page ?></span>
-                оскорблений из
-                <span class="font-medium"><?= $total_rows ?></span>
+                оскорблений на странице.
+                <span class="font-medium"><?= $pageno ?></span>
+                страница из
+                <span class="font-medium"><?= $total_pages ?></span>
             </p>
         </div>
         <div>
             <nav class="relative z-0 inline-flex shadow-sm -space-x-px" aria-label="Pagination">
-                <a href="<?php if($pageno <= 1){ echo '#'; } else { echo '?pageno='.($pageno - 1); } ?>" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                <a <?php if($pageno <= 1){echo ''; } else {?>href="<?php echo '?pageno='.($pageno - 1); } ?>" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                     <span class="sr-only">Предыдущая</span>
                     <!-- Heroicon name: chevron-left -->
                     <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                     </svg>
                 </a>
-                <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo '?pageno='.($pageno + 1); } ?>" class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                <a <?php if($pageno >= $total_pages){ echo ''; } else { ?> href="<?php echo '?pageno='.($pageno + 1); } ?>" class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                     <span class="sr-only">Следующая</span>
                     <!-- Heroicon name: chevron-right -->
                     <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
