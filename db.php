a5c9680bd2ed5404e6882eb8da230a23c26b4207
m.morshinin <m.morshinin@b2b-center.ru>
2021-02-04 18:08:39 +0300

    main did something with heroku

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-include_once ('config.php');
+include ('config.php');
 
 try {
     $dsn = 'pgsql:host='. $host . ';port='. $port
@@ -14,24 +14,3 @@ try {
 catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
-//$dbopts = parse_url(getenv('DATABASE_URL'));
-//$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-//    array(
-//        'pdo.server' => array(
-//            'driver'   => 'pgsql',
-//            'user' => $dbopts["user"],
-//            'password' => $dbopts["pass"],
-//            'host' => $dbopts["host"],
-//            'port' => $dbopts["port"],
-//            'dbname' => ltrim($dbopts["path"],'/')
-//        )
-//    )
-//);
-//$host = 'localhost';
-//$user = 'postgres';
-//$password = 'Q3mvtobs';
-//$dbname = 'insults';
-//
-//$dsn = 'pgsql:host=' . $host . ';dbname=' . $dbname;
-//
-//$pdo = new PDO($dsn, $user, $password);
