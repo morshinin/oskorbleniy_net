ba440979d4ee8da27f437166ddaa1916043cd633
m.morshinin <m.morshinin@b2b-center.ru>
2021-02-06 23:33:10 +0300

    f_hide-db-credentials another test parse database url

--- classes/Dbh.class.php
+++ classes/Dbh.class.php
@@ -20,24 +20,23 @@ class Dbh {
     public function dbConnection()
     {
         $parts = (parse_url(getenv('DATABASE_URL')));
-//        extract($parts);
-        var_dump(getenv('DATABASE_URL'));
+        extract($parts);
+        var_dump($parts);
     }
 
     public function connect()
     {
+        $this->dbConnection();
 
         try {
 //            $dsn = 'pgsql:host='. $this->host . ';port='. $this->port
-            echo 'db connect';
-            $this->dbConnection();
 //                . ';dbname='. $this->dbname. ';user='. $this->user
 //                . ';password='. $this->password;
 //
-//            $pdo = new PDO($dsn, $this->user, $this->password);
-//            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-//            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-//            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//            $pdo = new pdo($dsn, $this->user, $this->password);
+//            $pdo->setattribute(pdo::attr_default_fetch_mode, pdo::fetch_assoc);
+//            $pdo->setattribute(pdo::attr_emulate_prepares, false);
+//            $pdo->setattribute(pdo::attr_errmode, pdo::errmode_exception);
 //
 //            return $pdo;
         }
