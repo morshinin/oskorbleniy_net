ea798bd7fa27590d4b08c8b83602884a012f1726
m.morshinin <m.morshinin@b2b-center.ru>
2021-02-06 23:21:37 +0300

    f_hide-db-credentials test parse database_url

--- classes/Dbh.class.php
+++ classes/Dbh.class.php
@@ -1,25 +1,45 @@
 <?php
 
 class Dbh {
-    private $host = 'ec2-3-214-4-151.compute-1.amazonaws.com';
-    private $user = 'pcznwpxkncqmow';
-    private $password = '62f6fd144dd256197f0545258f28245cadf0d17d54b3d510b181fda5db599317';
-    private $dbname = 'd8vq4946vp4h8p';
-    private $port = '5432';
+    private $host;
+    private $user;
+    private $password;
+    private $dbname;
+    private $port;
+    private $path;
+
+//    public function setDbCredentials()
+//    {
+//        $this->host = getenv('HOST');
+//        $this->user = getenv('USER');
+//        $this->password = getenv('PASSWORD');
+//        $this->dbname = getenv('DBNAME');
+//        $this->port = getenv('PORT');
+//    }
+
+    public function dbConnection()
+    {
+        $parts = (parse_url(getenv('DATABASE_URL')));
+//        extract($parts);
+        var_dump(getenv('DATABASE_URL'));
+    }
 
     public function connect()
     {
+
         try {
-            $dsn = 'pgsql:host='. $this->host . ';port='. $this->port
-                . ';dbname='. $this->dbname. ';user='. $this->user
-                . ';password='. $this->password;
-
-            $pdo = new PDO($dsn, $this->user, $this->password);
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            return $pdo;
+//            $dsn = 'pgsql:host='. $this->host . ';port='. $this->port
+            echo 'db connect';
+            $this->dbConnection();
+//                . ';dbname='. $this->dbname. ';user='. $this->user
+//                . ';password='. $this->password;
+//
+//            $pdo = new PDO($dsn, $this->user, $this->password);
+//            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+//            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+//            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//
+//            return $pdo;
         }
         catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
