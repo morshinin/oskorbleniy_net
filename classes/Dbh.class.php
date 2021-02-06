93d9300a39d138c4c8e20be5616931d457d16044
m.morshinin <m.morshinin@b2b-center.ru>
2021-02-07 00:21:54 +0300

    f_hide-db-credentials db connection from config var database_url

--- classes/Dbh.class.php
+++ classes/Dbh.class.php
@@ -21,7 +21,11 @@ class Dbh {
     {
         $parts = (parse_url(getenv('DATABASE_URL')));
         extract($parts);
-        var_dump($parts);
+        $this->host = $parts['host'];
+        $this->port = $parts['port'];
+        $this->user = $parts['user'];
+        $this->password = $parts['pass'];
+        $this->dbname = $parts['path'];
     }
 
     public function connect()
@@ -29,16 +33,16 @@ class Dbh {
         $this->dbConnection();
 
         try {
-//            $dsn = 'pgsql:host='. $this->host . ';port='. $this->port
-//                . ';dbname='. $this->dbname. ';user='. $this->user
-//                . ';password='. $this->password;
-//
-//            $pdo = new pdo($dsn, $this->user, $this->password);
-//            $pdo->setattribute(pdo::attr_default_fetch_mode, pdo::fetch_assoc);
-//            $pdo->setattribute(pdo::attr_emulate_prepares, false);
-//            $pdo->setattribute(pdo::attr_errmode, pdo::errmode_exception);
-//
-//            return $pdo;
+            $dsn = 'pgsql:host='. $this->host . ';port='. $this->port
+                . ';dbname='. $this->dbname. ';user='. $this->user
+                . ';password='. $this->password;
+
+            $pdo = new pdo($dsn, $this->user, $this->password);
+            $pdo->setattribute(pdo::attr_default_fetch_mode, pdo::fetch_assoc);
+            $pdo->setattribute(pdo::attr_emulate_prepares, false);
+            $pdo->setattribute(pdo::attr_errmode, pdo::errmode_exception);
+
+            return $pdo;
         }
         catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
